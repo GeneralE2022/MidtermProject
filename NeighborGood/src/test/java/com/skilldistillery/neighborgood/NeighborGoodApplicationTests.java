a093d8394f41c2740b940b5a7951a847656a63b1
Erich Schindler <erschindler@gmail.com>
2022-05-19 16:19:53 -0400

    Create Boot MVC project dependency

---
+++ NeighborGood/src/test/java/com/skilldistillery/neighborgood/NeighborGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.neighborgood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighborGoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
