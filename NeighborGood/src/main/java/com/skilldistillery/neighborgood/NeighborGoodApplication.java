8d73595ad598e58afad80dd8aa8ed4b5aad65395
paul-chaffin <pechaffin@pm.me>
2022-05-26 17:26:53 -0400

    deploy safety commit

--- NeighborGood/src/main/java/com/skilldistillery/neighborgood/NeighborGoodApplication.java
+++ NeighborGood/src/main/java/com/skilldistillery/neighborgood/NeighborGoodApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.neighborgood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NeighborGoodApplication {
+public class NeighborGoodApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(NeighborGoodApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(NeighborGoodApplication.class, args);
